30307e5cd5e8a63a0a35dd6d6c28272a3593507a
Turisap <kirill-shakirov@mail.ru>
2017-10-28 20:56:11 +0700

    Installed babel

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 /**
  * Created by HP on 28-Oct-17.
  */
-const sea = document.querySelector('#window');
+import Gun from './src/gun';
+/*const sea = document.querySelector('#window');
 const gun = document.querySelector('#gun');
 
 function rotateGun(e){
+    //debugger;
     const angle = (e.pageX / window.innerWidth) * 180 - 180;
-    //console.warn(gun);
     gun.style.webkitTransform = `rotate(${angle}deg)`
 }
 
-document.body.addEventListener('mousemove', rotateGun);
+document.body.addEventListener('mousemove', rotateGun);*/
+
+const g = new Gun();
+g.getDistance();
---
+++ src/gun.js
@@ -0,0 +1,13 @@
+/**
+ * Created by HP on 28-Oct-17.
+ */
+export default class  Gun {
+    constructor() {
+
+    }
+
+    getDistance() {
+        console.log('Distande');
+    }
+}
+
